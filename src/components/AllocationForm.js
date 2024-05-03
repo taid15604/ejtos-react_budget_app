0117821687d3a7fe6735628039f868ab20cf7519
taid15604 <taid15604@gmail.com>
2024-05-03 15:32:48 -0400

    Currently@ Task 2 Last task, Should not allow for the budget to be lower thatn the spending etc.

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -58,13 +58,15 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-
+                  <div className="input-group-prepend" style={{ marginLeft: '2rem' }}>
+                    <label className="input-group-text" htmlFor="cost">£</label>
+                  </div>
+                  
                     <input
                         required='required'
                         type='number'
                         id='cost'
                         value={cost}
-                        style={{ marginLeft: '2rem' , size: 10}}
                         onChange={(event) => setCost(event.target.value)}>
                         </input>
 
